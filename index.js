aae4c0145435cb444d81a976ca0049f5c07ca276
dlav01 <iavdivad@gmail.com>
2018-06-23 18:27:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@
     _getCustomFiles () {
 
       return $.extend ( super._getCustomFiles (), {
-        'map_forest.jpg': 'map_forest_grid.jpg',
+        'map_forest.jpg': 'map_forest_grid.png',
         'map_rock_mask.jpg': 'map_rock_mask_grid.jpg',
         'map_sand_mask.jpg': 'map_sand_mask_grid.jpg',
         'map_sea.jpg': 'map_sea_grid.jpg',
