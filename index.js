c48105e3cc95e859712a0941a98bb0da69f38273
dlav01 <iavdivad@gmail.com>
2018-08-18 22:04:54 +1000

    Update index.js

--- index.js
+++ index.js
@@ -154,6 +154,7 @@
     _getCustomFiles () {
 
       return $.extend ( super._getCustomFiles (), {
+        'gui.png': 'gui.png',
         'map_forest.jpg': 'map_forest_grid.jpg',
         'map_rock_mask.jpg': 'map_rock_mask_grid.jpg',
         'map_sand_mask.jpg': 'map_sand_mask_grid.jpg',
